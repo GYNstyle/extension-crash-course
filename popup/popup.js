036af6a90fd0009a0cf364276e6445c0080be24f
guoguo <guoguo@Yifus-MacBook-Air.local>
2023-08-14 16:26:27 +0800

    Add chrome tabs query in popup js

--- popup/popup.js
+++ popup/popup.js
@@ -4,8 +4,19 @@ document.querySelector("#btn").addEventListener("click", () => {
   chrome.runtime.sendMessage({
     text: "Popup",
   });
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      chrome.tabs.sendMessage(tabs[0]?.id, {
+        text: "Popup",
+      });
+    }
+  );
 });
 
 chrome.runtime.onMessage.addListener((msg) => {
-    document.body.innerHTML += `<div>${msg.text}</div>`
-})
+  document.body.innerHTML += `<div>${msg.text}</div>`;
+});
